6d85b546bb85f83b55f44a0ab30caf135f5531b0
Alex Kaminski <alexdkaminski@gmail.com>
2021-04-19 15:13:15 +1000

    Remove job description

--- src/components/job.js
+++ src/components/job.js
@@ -108,14 +108,6 @@ const Job = ({ job }) => {
                 ${job.quote}
               </dd>
             </div>
-            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <dt className="text-sm leading-5 font-medium text-gray-500">
-                Description
-          </dt>
-              <dd className="mt-1 text-sm leading-5 text-gray-900 sm:mt-0 sm:col-span-2">
-                {job.description}
-              </dd>
-            </div>
           </dl>
         </div>
       </div>
